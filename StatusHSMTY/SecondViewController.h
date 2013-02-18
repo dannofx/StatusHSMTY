22886bd044e0cbecc203d284cf2ac93c5de6e4dc
Daniel Heredia <daniel.felipe@naranya.com>
2013-02-18 17:07:46 -0600

    Initial Commit

---
+++ StatusHSMTY/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  StatusHSMTY
+//
+//  Created by Danno on 2/18/13.
+//  Copyright (c) 2013 Danno. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
